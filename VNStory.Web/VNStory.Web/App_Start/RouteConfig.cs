68335b2fcfbf1584a37be625ca6fb8029aa807d7
dohiep-neu <itachi2862001@gmail.com>
2022-03-31 20:09:28 +0700

    test comment

--- VNStory.Web/VNStory.Web/App_Start/RouteConfig.cs
+++ VNStory.Web/VNStory.Web/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace VNStory.Web
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            //test
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
