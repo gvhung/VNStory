b7d6b4bf33c41c44c58682b1e7b8c9dad422fc10
donam2k <dothenam2k@gmail.com>
2022-06-14 19:41:07 +0700

    newupdata

--- VNStory.Web/VNStory.Web/Areas/Admin/Controllers/ChapterController.cs
+++ VNStory.Web/VNStory.Web/Areas/Admin/Controllers/ChapterController.cs
@@ -61,6 +61,42 @@ namespace VNStory.Web.Areas.Admin.Controllers
 
         public ActionResult Details(string id, string storyId)
         {
+            if (string.IsNullOrEmpty(id) == true)
+            {
+                return HttpNotFound();
+            }
+
+            int _id = 0;
+
+            int.TryParse(id, out _id);
+
+            var chapterItem = db.Chapters.Find(_id);
+
+            if (chapterItem == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (string.IsNullOrEmpty(storyId) == false)
+            {
+                int _storyId = 0;
+
+                int.TryParse(storyId, out _storyId);
+
+                if (chapterItem.StoryId != _storyId)
+                {
+                    return HttpNotFound();
+                }
+                else
+                {
+                    var story = db.Stories.Find(_storyId);
+                    if (story != null)
+                    {
+                        chapterItem.Story = story;
+                    }
+                }
+                 
+            }
             var chapter = db.Chapters.Find(int.Parse(id));
 
             if (chapter == null)
@@ -137,38 +173,88 @@ namespace VNStory.Web.Areas.Admin.Controllers
             return RedirectToAction("Index", new { storyId = storyId });
         }
 
+        //public ActionResult Edit(string id, string storyId)
+        //{
+        //    var chapterItem = db.Chapters.Find(int.Parse(id));
+
+        //    if (chapterItem == null)
+        //    {
+        //        //Thông báo không tìm thấy chương
+        //        return HttpNotFound();
+        //    }
+
+        //    //Kiểm tra xem parameter storyId trên Url hay không
+        //    if (string.IsNullOrEmpty(storyId) == false)
+        //    {
+        //        //Kiểm tra xem giá trị StoryId của cương có bằng giá trị của param StoryId trên Url hay không
+        //        if (chapterItem.StoryId != int.Parse(storyId))
+        //        {
+        //            //giá trị StoryId không bằng nhau
+        //            return HttpNotFound();
+        //        }
+        //        else
+        //        {
+        //            //Lấy đối tượng truyện có mã Id = giá trị của param StoryId trên Url
+        //            var story = db.Stories.Find(int.Parse(storyId));
+        //            if (story != null)
+        //            {
+        //                chapterItem.Story = story;
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        //Lấy đối tượng truyện có mã Id = giá trị của StoryId thuộc đối tượng Chương
+        //        var story = db.Stories.Find(chapterItem.StoryId);
+        //        if (story != null)
+        //        {
+        //            chapterItem.Story = story;
+        //        }
+        //    }
+
+        //    return View(chapterItem);
+        //}
+
         public ActionResult Edit(string id, string storyId)
         {
-            var chapterItem = db.Chapters.Find(int.Parse(id));
-
-            if (chapterItem == null)
+            if (string.IsNullOrEmpty(id) == true)
             {
-                //Thông báo không tìm thấy chương
                 return HttpNotFound();
             }
 
-            //Kiểm tra xem parameter storyId trên Url hay không
+            int _id = 0;
+
+            int.TryParse(id, out _id);
+
+            var chapterItem = db.Chapters.Find(_id);
+
+            if (chapterItem == null)
+            {
+                return HttpNotFound();
+            }
+
             if (string.IsNullOrEmpty(storyId) == false)
             {
-                //Kiểm tra xem giá trị StoryId của cương có bằng giá trị của param StoryId trên Url hay không
-                if (chapterItem.StoryId != int.Parse(storyId))
+                int _storyId = 0;
+
+                int.TryParse(storyId, out _storyId);
+
+                if (chapterItem.StoryId != _storyId)
                 {
-                    //giá trị StoryId không bằng nhau
                     return HttpNotFound();
                 }
                 else
                 {
-                    //Lấy đối tượng truyện có mã Id = giá trị của param StoryId trên Url
-                    var story = db.Stories.Find(int.Parse(storyId));
+                    var story = db.Stories.Find(_storyId);
                     if (story != null)
                     {
                         chapterItem.Story = story;
                     }
                 }
+
             }
             else
             {
-                //Lấy đối tượng truyện có mã Id = giá trị của StoryId thuộc đối tượng Chương
                 var story = db.Stories.Find(chapterItem.StoryId);
                 if (story != null)
                 {
@@ -178,6 +264,7 @@ namespace VNStory.Web.Areas.Admin.Controllers
 
             return View(chapterItem);
         }
+
 
         [HttpPost]
         public ActionResult Edit([Bind(Include = "Id,Title,Content,NumberChapter,StoryId")] Chapter chapter)
